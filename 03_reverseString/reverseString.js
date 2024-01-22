451b343ecead5b6591df68b2d65e6ef724679c58
Jordan Stewart <81133588+JSordan22@users.noreply.github.com>
2024-01-22 17:02:39 -0600

    completed 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reverseStr = "";
+    
+    for(let index = str.length - 1; index >= 0; index--) {
+        reverseStr += str.substring(index, index + 1);
+    }
 
+    return reverseStr;
 };
 
 // Do not edit below this line
