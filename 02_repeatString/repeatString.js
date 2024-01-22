f74c993afae107b28b26b0213bee88822f38ef3b
Jordan Stewart <81133588+JSordan22@users.noreply.github.com>
2024-01-22 16:51:14 -0600

    completed 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let outcome = "";
+    
+    for(let count = 0; count < num; count++) {
+        outcome += str;
+    }
 
+    if(num < 0) {
+        return "ERROR";
+    }
+    return outcome;
 };
 
 // Do not edit below this line
