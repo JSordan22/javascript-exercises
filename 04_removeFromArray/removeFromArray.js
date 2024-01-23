40a95201ceb6617b6a457fb4250a812ef5dd13bc
Jordan Stewart <81133588+JSordan22@users.noreply.github.com>
2024-01-22 20:11:47 -0600

    completed 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {//splice and nested for loop
+    for(let count = 1; count < arguments.length; count++) {
+        for(let i = 0; i < arr.length; i++) {
+            if(arr[i] === arguments[count]) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
