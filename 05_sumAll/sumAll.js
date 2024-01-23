54210dfc2c1b027a357842545f6b08f1d8325700
Jordan Stewart <81133588+JSordan22@users.noreply.github.com>
2024-01-23 16:10:11 -0600

    completed 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let sum = 0;
 
+    for(let i = Math.min(int1, int2); i <= Math.max(int1, int2); i++) {
+        sum += i;
+    }
+    
+    if(!(int1 >= 0) || !(int2 >= 0) &&
+       !(typeof int1 === "number") || !(typeof int2 === "number")) {
+        return "ERROR";
+    }
+    return sum;
 };
 
 // Do not edit below this line
