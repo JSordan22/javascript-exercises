6f7b976efdb14f95daa614176ac0086d2c5aef85
Jordan Stewart <81133588+JSordan22@users.noreply.github.com>
2024-10-08 13:03:55 -0500

    Finished sum function in calc

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,8 @@ const subtract = function(num1, num2) {
 	return Math.abs(num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, num) => acc + num, 0);
 };
 
 const multiply = function() {
