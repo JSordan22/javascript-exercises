07703a536360fe72eb467112123f8410dce3cdb2
Jordan Stewart <81133588+JSordan22@users.noreply.github.com>
2024-10-09 11:36:10 -0500

    completed 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let modStr = str.toLowerCase().split("");
+    let badElements = [",", " ", "!", "?", "."]
+    let cleanStr = modStr.filter((ltr) => badElements.indexOf(ltr) < 0);
+    
+    return cleanStr.join("") == cleanStr.reverse().join("");
 };
 
 // Do not edit below this line
