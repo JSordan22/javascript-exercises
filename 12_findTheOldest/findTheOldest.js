5271cc4bfc7a51c52cf50c1599a82a62c003b807
Jordan Stewart <81133588+JSordan22@users.noreply.github.com>
2024-10-09 13:35:25 -0500

    completed 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const date = new Date();
+    let max = arr[0];
+    let maxAge = max.yearOfDeath == undefined ? date.getFullYear() - max.yearOfBirth : max.yearOfDeath - max.yearOfBirth;
+    
+    for (let i = 0; i < arr.length; i++) {
+        if(max.yearOfDeath == undefined) {
+            if(maxAge < arr[i].yearOfDeath - arr[i].yearOfBirth) {
+                max = arr[i];
+                maxAge = arr[i].yearOfDeath - arr[i].yearOfBirth;
+            }
+        } else if(arr[i].yearOfDeath == undefined) {
+            if(maxAge < date.getFullYear() - arr[i].yearOfBirth) {
+                max = arr[i];
+                maxAge = date.getFullYear() - arr[i].yearOfBirth;
+            }
+        } else {
+            if(maxAge < arr[i].yearOfDeath - arr[i].yearOfBirth) {
+                max = arr[i];
+                maxAge = arr[i].yearOfDeath - arr[i].yearOfBirth;
+            }
+        }
+    }
+    return max;
 };
 
 // Do not edit below this line
