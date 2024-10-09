a45866b5fe63250e6f4c9962fb3bbde2561b0bd8
Jordan Stewart <81133588+JSordan22@users.noreply.github.com>
2024-10-09 12:45:29 -0500

    completed 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(pos) {
+    if(pos == 0) {
+        return 0;
+    }
+    else if(pos == 1) {
+        return 1;
+    }
+    else if(pos < 0) {
+        return "OOPS";
+    }
+    return fibonacci(pos - 2) + fibonacci(pos - 1);
 };
 
 // Do not edit below this line
